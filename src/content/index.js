deb22f5711ff2e30ee21320d5ef96b2b70f9f0cc
cafe4it <cafe4it@gmail.com>
2016-03-11 23:26:53 +0700

    add new icons

--- src/content/index.js
+++ src/content/index.js
@@ -1,6 +1,7 @@
 import _ from 'lodash';
 import './index.css';
-const ICON_BUTTON_DOWNLOAD_ALL = require('../icons/buttonDownloadAll.png');
+const ICON_BUTTON_DOWNLOAD_ALL = require('../icons/webpack-19.png');
+const ICON_BUTTON_DOWNLOAD_ALL2 = require('../icons/webpack-32.png');
 
 $('#img').on('mouseenter', 'a.ui-magnifier-glass', function (e) {
     $(this).on('contextMenu', function () {
@@ -25,7 +26,7 @@ if (window.location.href.match(detailProductRegex)) {
     var h1 = $('h1.product-name')[0];
     var productId = window.location.href.split('/').pop().match(/(.*)\.html/)[1] || $('#hid-product-id')[0];
     if(h1 && productId){
-        var buttonDownloadAll = buildDownloadAllButton(productId.value, window.location.href, 'buttonDownloadALl_detail');
+        var buttonDownloadAll = buildDownloadAllButton(productId.value, window.location.href, 'buttonDownloadALl_detail', ICON_BUTTON_DOWNLOAD_ALL2);
         var span = document.createElement('span');
         span.appendChild(buttonDownloadAll);
         h1.parentNode.insertBefore(span, h1.nextSibling);
@@ -40,7 +41,7 @@ $('.list-items, .items-list, #list-items, .list-box, #p4p-ul-content, #bestselli
         var topChildNode = $(container).find('div.img, .img-box,.img-wrap,.ui-product-listitem-thumb')[0],
             linkToDetail = $(container).find("a")[0],
             productId = linkToDetail.href.split('/').pop().match(/(.*)\.html/)[1];
-        console.log(topChildNode, linkToDetail, productId);
+        //console.log(topChildNode, linkToDetail, productId);
         if (topChildNode && linkToDetail && productId) {
             buttonDownloadALl = buildDownloadAllButton(productId, linkToDetail.href, 'buttonDownloadALl');
             container.insertBefore(buttonDownloadALl, topChildNode);
@@ -53,11 +54,13 @@ $('.list-items, .items-list, #list-items, .list-box, #p4p-ul-content, #bestselli
     })
 });
 
-function buildDownloadAllButton(productId, href, className) {
+function buildDownloadAllButton(productId, href, className,src) {
     var className = className || '';
+    var imageSrc = src || ICON_BUTTON_DOWNLOAD_ALL;
     var buttonDownloadALl = document.createElement('img');
-    buttonDownloadALl.src = ICON_BUTTON_DOWNLOAD_ALL;
+    buttonDownloadALl.src = imageSrc;
     buttonDownloadALl.className = className;
+    buttonDownloadALl.setAttribute('alt', chrome.i18n.getMessage('buttonDownloadAll'))
     $(buttonDownloadALl).on('click', function (e) {
         chrome.runtime.sendMessage({
             action: 'DOWNLOAD_ALL_IMAGES',
