7e143efb2cb52688947d4e75b316a7618540f47b
Nguyen Xuan Cong <nguyenxuancong@outlook.com>
2016-03-11 16:56:36 +0700

    done 95%

--- src/content/index.js
+++ src/content/index.js
@@ -2,43 +2,48 @@ import _ from 'lodash';
 import './index.css';
 const ICON_BUTTON_DOWNLOAD_ALL = require('../icons/buttonDownloadAll.png');
 
-$('#img').on('mouseenter', 'a.ui-magnifier-glass', function(e){
-    $(this).on('contextMenu', function(){
-        if($(e.relatedTarget).is('img')){
+$('#img').on('mouseenter', 'a.ui-magnifier-glass', function (e) {
+    $(this).on('contextMenu', function () {
+        if ($(e.relatedTarget).is('img')) {
             var src = e.relatedTarget.src;
-            if(src){
+            if (src) {
                 chrome.runtime.sendMessage({
-                    action : 'CONTEXT_MENUS_PER_IMAGE',
-                    data : {
-                        option : 1,
-                        src : src
+                    action: 'CONTEXT_MENUS_PER_IMAGE',
+                    data: {
+                        option: 1,
+                        src: src
                     }
                 })
             }
         }
     })
-})
+});
 
-$('.list-items').on('mouseenter', 'li.list-item', function (e) {
-    var container = $(this).find('div.item')[0];
+var detailProductRegex = /www\.aliexpress\.com\/item/g;
+
+if (window.location.href.match(detailProductRegex)) {
+    var h1 = $('h1.product-name')[0];
+    var productId = window.location.href.split('/').pop().match(/(.*)\.html/)[1] || $('#hid-product-id')[0];
+    if(h1 && productId){
+        var buttonDownloadAll = buildDownloadAllButton(productId.value, window.location.href, 'buttonDownloadALl_detail');
+        var span = document.createElement('span');
+        span.appendChild(buttonDownloadAll);
+        h1.parentNode.insertBefore(span, h1.nextSibling);
+    }
+}
+
+$('.list-items, .items-list, #list-items, .list-box, #p4p-ul-content, #bestselling-top10, .items, .ui-box-body').on('mouseenter', '.item, .img-box-wrap,li.top10-item, .img-container, .ui-product-listitem', function (e) {
+    var container = this;
+    var buttonDownloadALl = null;
+    //console.log($(this))
     if (container) {
-        var buttonDownloadALl = document.createElement('img');
-        buttonDownloadALl.src = ICON_BUTTON_DOWNLOAD_ALL;
-        buttonDownloadALl.className = 'buttonDownloadALl';
-        var topChildNode = $(container).find('div.img.img-border')[0],
-            linkToDetail = $(container).find('a.picRind')[0],
-            productId = $(container).find('input.atc-product-id')[0];
+        var topChildNode = $(container).find('div.img, .img-box,.img-wrap,.ui-product-listitem-thumb')[0],
+            linkToDetail = $(container).find("a")[0],
+            productId = linkToDetail.href.split('/').pop().match(/(.*)\.html/)[1];
+        console.log(topChildNode, linkToDetail, productId);
         if (topChildNode && linkToDetail && productId) {
+            buttonDownloadALl = buildDownloadAllButton(productId, linkToDetail.href, 'buttonDownloadALl');
             container.insertBefore(buttonDownloadALl, topChildNode);
-            $(buttonDownloadALl).on('click', function (e) {
-                chrome.runtime.sendMessage({
-                    action : 'DOWNLOAD_ALL_IMAGES',
-                    data : {
-                        href : linkToDetail.href,
-                        pId : productId.value
-                    }
-                })
-            })
         }
     }
     $(this).on('mouseleave', function () {
@@ -47,5 +52,22 @@ $('.list-items').on('mouseenter', 'li.list-item', function (e) {
         }
     })
 });
+
+function buildDownloadAllButton(productId, href, className) {
+    var className = className || '';
+    var buttonDownloadALl = document.createElement('img');
+    buttonDownloadALl.src = ICON_BUTTON_DOWNLOAD_ALL;
+    buttonDownloadALl.className = className;
+    $(buttonDownloadALl).on('click', function (e) {
+        chrome.runtime.sendMessage({
+            action: 'DOWNLOAD_ALL_IMAGES',
+            data: {
+                href: href,
+                pId: productId
+            }
+        })
+    });
+    return buttonDownloadALl;
+}
 
 
